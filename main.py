9c43ffe439f77a3f9f9ad7b787f211bb7f68d8e3
Ho Nyeong <dev.honing@gmail.com>
2024-02-26 11:30:51 +0900

    Fix: 줄바꿈 및 주석 수정

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+# main.py
+
 # json import
 # json을 사용하려면 node.js와 다르게 import가 필요
 import json
 
-# 예제를 위한 임의의 json 파일 경로
+# 예제를 위한 JSON 파일 경로를 변수 선언
 file_path = 'data/data.json'
 
 # 함수 작성해보기
@@ -14,6 +16,7 @@ def delete_function(data):
     # 내용
     return
 
+# JSON 생성 함수
 def create_json(data):
     # open 함수는 파일을 열 때 쓴다. 첫번째 인자로는 '파일의 경로'를, 두번째 인자로는 '모드'를 지정한다.
     # 'w'는 write의 줄임말로, 보통 '모드'라고 불리며, 'w'는 파일을 쓰기 위해 연다.(없으면 새로 생성)
@@ -21,6 +24,12 @@ def create_json(data):
         # json.dump 메서드는 Python 데이터를 JSON 형식의 문자열로 변환해 파일에 쓴다.
         # 여기서는 data 변수의 내용을 JSON 형태로 파일에 저장한다.
         json.dump(data, file, indent=4)
+
+# JSON에 추가할 데이터 변수 선언
+data_to_write = {"name": "BHN", "birth": 1997, "age": 26, "city": "Daejeon"}
+
+# create_json 함수 호출 및 데이터 추가
+create_json(data_to_write)
 
 def read_json():
     try:
@@ -36,10 +45,3 @@ def read_json():
         # None 데이터 타입은 JavaScript의 null과 비슷하다.
         return None
     
-
-
-# json에 추가할 데이터 변수 선언
-data_to_write = {"name": "BHN", "birth": 1997, "age": 26, "city": "Daejeon"}
-
-# create_json 함수 호출 및 데이터 추가
-create_json(data_to_write)
