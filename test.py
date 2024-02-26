642465cf0eb7218643e582d142dbeb6c3cb238c9
Ho Nyeong <dev.honing@gmail.com>
2024-02-26 13:10:16 +0900

    Feat: 파이썬의 반복문 알아보기

---
+++ test.py
@@ -0,0 +1,10 @@
+# 파이썬의 반복문
+# 리스트
+list = [1, 2, 3, 4, 5]
+for item in list:
+    print(item)
+
+# 딕셔너리
+dict = {'name': 'BHN', 'birth': 1997, 'age': 26, 'city': 'Daejeon'}
+for key, value in dict.items():
+    print(key, value)
