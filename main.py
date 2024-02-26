37b1f546dafafe5233b4e05fd0556be83ac47928
Ho Nyeong <dev.honing@gmail.com>
2024-02-26 11:28:00 +0900

    Comments: 'r' 모드와 read_json() 함수 부연 설명

--- main.py
+++ main.py
@@ -23,11 +23,20 @@ def create_json(data):
         json.dump(data, file, indent=4)
 
 def read_json():
-    try: 
+    try:
+        # 'r' 모드는 파일을 읽기 위해 쓴다.
+        # Node.js에서는 readFile(), readFileSync() 메서드를 사용하지만, '모드' 방식 덕분에 open() 메서드로 통일해 사용할 수 있다.
+        # with as 문법을 사용하면, 파일을 열고 닫는 것을 자동으로 처리할 수 있다.
+        # '연다'와 '닫는다'를 명시적으로 사용하는 것이 Python의 장점 중 하나이다.
         with open(file_path, 'r') as file:
+            # json.load() 메서드는 파일의 내용을 JSON으로 파싱하여 Python 데이터 타입으로 변환한다.
             return json.load(file)
     except FileNotFoundError:
+        # except는 예외가 발생했을 때 사용하는 JavaScript의 try-catch와 비슷한 문법이다.
+        # None 데이터 타입은 JavaScript의 null과 비슷하다.
         return None
+    
+
 
 # json에 추가할 데이터 변수 선언
 data_to_write = {"name": "BHN", "birth": 1997, "age": 26, "city": "Daejeon"}
